cd779637645dca2345c1bddfef0aa745effed57c
jdpython <jdpython46@gmail.com>
2025-02-24 15:28:28 -0800

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Jimmy Diaz on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
