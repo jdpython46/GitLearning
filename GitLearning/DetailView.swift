dfb551fc31f1ea2b8b98842954fa9b92168b6da6
jdpython <jdpython46@gmail.com>
2025-02-24 15:58:55 -0800

    Added navigationlink to ContentView and added DetailView

---
+++ GitLearning/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  GitLearning
+//
+//  Created by Jimmy Diaz on 2/24/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("An App to Learn Git") 
+    }
+}
+
+#Preview {
+    DetailView()
+}
